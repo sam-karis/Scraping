c61f21adeae5227e1a9289fb488e62f69d26bfa7
sam-karis <samkaris75@gmail.com>
2019-12-04 04:08:31 +0300

    fix bytes error and add .gitignore

--- scraping/web-s.py
+++ scraping/web-s.py
@@ -5,20 +5,20 @@ import csv
 url = 'https://report.boonecountymo.org/mrcjava/servlet/RMS01_MP.I00030s'
 response = requests.get(url, headers={'User-Agent': 'Chrome/78.0.3904.108'})
 html = response.content
-print(html)
 
 soup = BeautifulSoup(html, 'html.parser')
+columns = [column.text.strip() for column in soup.select('.data-table .table-header th')]
 table = soup.find('tbody', attrs={'class': 'stripe'})
 
 list_of_rows = []
 for row in table.findAll('tr'):
     list_of_cells = []
     for cell in row.findAll('td'):
-        text = cell.text.replace('&nbsp;', '')
+        text = cell.text.strip().replace('&nbsp;', '')
         list_of_cells.append(text)
         list_of_rows.append(list_of_cells)
-    print (list_of_rows)
 
-outfile = open("./inmates.csv", "r+")
-writer = csv.writer(outfile)
-writer.writerows(list_of_rows)
+with open("./inmates.csv", 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(columns)
+    writer.writerows(list_of_rows)
